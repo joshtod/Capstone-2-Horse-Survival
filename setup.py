5b37390f41e1e19398ba41eed6ef6910b82dea54
joshtod <134558203+joshtod@users.noreply.github.com>
2023-09-14 16:33:30 -0500

    Second capstone, first submission: Data Wrangling

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Kaggle competiton, possibly also used as a Springboard capstone.',
+    author='Joshua Ogden-Davis',
+    license='MIT',
+)
